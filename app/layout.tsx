faf657d890eefcfb34e157c895cf0665ff7de8af
Dmitriy <87428235+DmitriySido@users.noreply.github.com>
2026-04-02 19:46:57 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,10 +21,8 @@ export default function RootLayout({
   return (
     <html lang="en" className={montserrat.variable}>
       <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </Head>
       <body className="antialiased">
         {children}
